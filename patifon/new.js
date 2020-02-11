eeb8ba52115aad6ad66005cd792a992788414978
Stepan82-st <bryl.step@tut.by>
2020-02-11 15:28:09 +0200

    Add files via upload

--- patifon/new.js
+++ patifon/new.js
@@ -11,41 +11,41 @@
 	function F(e) {
 		if (e.target === start) {
 			//duda.style.visibility = "visible";
-			duda.style.transform = "rotate(190deg)";
-			disk.style.transform = "rotate(20deg)";
-
-			TweenLite.to(disk, 10, {
-				rotation: "360deg",
+			//duda.style.transform = "rotate(185deg)";
+			TweenLite.to(duda,0,{
+				rotation:"185deg",
 				onComplete: F1
+				});
+			TweenLite.to(disk, 1000, {
+				rotation: "12800deg"
+				//onComplete: F1
 
 			});
-			function F1() {
-				TweenLite.to(disk, 100, {
-					rotation: "1280deg",
-					onComplete: F2
-				});
+			function F1(){
+			TweenLite.to(duda, 100, {
+					rotation: "205deg"
+					//onComplete: F2
+				});}
 			}
-			function F2() {
-				TweenLite.to(disk, 200, {
-					rotation: "2360deg",
-					onComplete: F3
-				});
-			}
-			function F3() {
-				TweenLite.to(disk, 3, {
-					rotation: "0deg",
-					onComplete: F1
-				});
-			}
-		} else if (e.target === pause) {
+		 else if (e.target === pause) {
 			  var x = disk.style.transform;
+              var y = duda.style.transform;
 			//duda.style.transform = "rotate(170deg)";
 			TweenLite.to(disk,0, {
 				transform: x
-
+			});
+			TweenLite.to(duda,0, {
+				transform: y
 			});
 		} else if (e.target === stop) {
-			duda.style.transform = "rotate(170deg)";
+			  var x = disk.style.transform;
+			TweenLite.to(duda,0,{
+				rotation:"170deg"
+				});
+			//duda.style.transform = "rotate(170deg)";
+			TweenLite.to(disk,0, {
+				transform: x
+			});
 		} else {
 			duda.style.transform = "rotate(170deg)";
 		}
@@ -75,7 +75,7 @@
 	function formBoxFun(e) {
 
 		var formElement = e.target;
-		while (!formElement.id) { // определение id родительского элемента "причастного" к событию
+		while (!formElement.id) { 
 			formElement = formElement.parentNode;
 		}
 
